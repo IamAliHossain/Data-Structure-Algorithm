1e4dcd133826bb89b959d0695bc32c0639a8826d
Ali Hossain <alihossain.cse.pstu@gamail.com>
2024-05-20 10:50:10 +0600

    Range update added

--- Segment Tree/segment_tree_code.cpp
+++ Segment Tree/segment_tree_code.cpp
@@ -5,6 +5,7 @@ const int N=3e5+7;
 typedef long long ll;
 ll a[N];
 ll st[N*4];
+ll lazy[N*4];
 
 void build_tree(int node, int b, int e){  /*node start from index 1, b-e means ei range e achi ekhon*/
     if(b == e){
@@ -60,6 +61,52 @@ void point_update(int node, int b, int e, int pos, int val){
     st[node] = (st[left] + st[right]);
 }
 
+void range_update(int node, int b, int e, int l, int r, int val){
+    
+    /* 
+        Find complete overlap, partial overlap , no overlap
+        if it is complete overlap--> then update the node and lazy propagate downward
+        if it is partial overlap--> then go left and right
+        if it has no overlap--> then just return to previos node 
+    */
+    
+    
+    // checking previous update remaing
+    if(lazy[node] != 0){
+        if(b != e){ // it has children left & right
+            st[node] += ((b-e+1) * lazy[node]);
+            int left = (node<< 1);
+            int right = (node << 1) + 1;
+            lazy[left] += lazy[node];
+            lazy[right] += lazy[node];
+        }
+        lazy[node] = 0;
+    }
+    // no overlap condition
+    if(e < l || r < b){ // no overlap condition
+        return;
+    }
+    if(b>= l and e <= r){ // complete overlap
+        st[node] = a[b] + val * (b - e + 1); // (total children * val) add hobe
+        
+        if(b != e){ // left & right children e propagate kora lagbe
+            int left = (node << 1);
+            int right = (node << 1) + 1;
+            lazy[left] += val;
+            lazy[right] += val;
+        }
+        return;
+    }
+    // for partial overlap what occurs are followed by
+    int left = (node << 1);
+    int right = (node << 1) + 1;
+    int mid = (b+e) >> 1;
+    range_update(left, b, mid, l, r, val);
+    range_update(right, mid+1, e, l, r, val);
+
+    st[node] = st[left] + st[right];
+
+}
 int main(){
     int t ;
     cin >> t ;
